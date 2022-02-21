c21354bbacf32811c7d33088c61f72ae05083e4e
Casper <100117982+CGP2209@users.noreply.github.com>
2022-02-21 12:01:47 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+        
+            namespace _Terning_
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Random value = new Random();
+                int terning = 0, seksere = 0, kast = 0;
+
+
+
+
+                terning = value.Next(1, 7);
+
+                Console.WriteLine("Terningen slår = {0} ", terning);
+
+
+
+                Console.ReadLine();
+
+
+
+
+
+
+            }
+        }
+    }
+
+    
+
